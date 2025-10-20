b1e02ebe5ee2a91385b994c7d420b9890d6565c5
Jacob Malland <mallandj@uwplatt.edu>
2025-10-19 19:35:40 -0500

    !!WIP!! - Created a custom component to display a "popup" screen.

---
+++ modules/PopupScreen.jsx
@@ -0,0 +1,34 @@
+import { Text, View, Component, Pressable } from 'react-native'
+import { useState } from 'react';
+
+const PopupScreen = ({ children, title, width, height, radius, style }) => {
+
+    return (
+        <View
+            style={{
+                width: width,
+                height: height,
+                display: 'flexbox',
+                borderRadius: radius,
+                borderWidth: 1,
+                borderColor: 'red',
+                backgroundColor: 'lightblue',
+                position: 'absolute',
+                zIndex: 1,
+                opacity: 0.8,
+            }}>
+
+            <Text style={{
+                textAlign: 'center',
+                color: 'black'
+            }}>{title}</Text>
+
+            <View>
+                {children}    
+            </View>
+        </View>
+    )
+}
+
+
+export default PopupScreen;
