450685b8f74d0ca702111d1f1271d7a7443d3277
Jacob Malland <mallandj@uwplatt.edu>
2025-10-21 13:06:41 -0500

    Accepted Kris' changes

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,10 @@
-import { StyleSheet, Text, View, Button, Image, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, FlatList, useWindowDimensions } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'expo-router';
-import TitleScreenButton from '../modules/TitleScreenButton';
-import PopupScreen from '../modules/PopupScreen';
-import ItemSelectMenu from '../modules/ItemSelectMenu';
 import { styles } from '../styles/main';
+import LevelSelect from '../modules/LevelSelect';
+import MainMenu from '../modules/MainMenu';
 
 // router.push(path): Navigates to a new screen and adds it to the navigation stack.
 // router.replace(path): Replaces the current screen in the navigation stack with the new one.
@@ -13,77 +12,26 @@ import { styles } from '../styles/main';
 // router.navigate({ pathname: string, params: object }): Navigates to a specific path with parameters.
 
 
-let image = () => {
-    return (
-        <Image
-            source={{ src: "https://images.dog.ceo/breeds/terrier-andalusian/images.jpg" }}
-            style={{
-                width: 85,
-                height: 85,
-                borderRadius: 10,
-                elevation: 15, // Android
-                shadowColor: "black",
-                shadowOpacity: 0.3,
-                shadowRadius: 4, // iOS
-            }}
-        />
-    )
-}
-
-const makeItems = (count) => {
-    let array = [];
-
-    for (let i=0; i<count; i++) {
-        array.push({
-            onPress: () => {
-                console.log("Pressed " + i);    
-            },
-            
-            icon: {
-                uri: "https://images.dog.ceo/breeds/terrier-andalusian/images.jpg"
-            },
-    
-            label: `${i}`,
-            
-            style: {
-                // size: 75,
-                // borderRadius: 10,
-                borderColor: "yellow",
-                // elevation: 10,
-                // shadowColor: "black",
-                // shadowOpacity: 0.3,
-                // shadowRadius: 4,
-                color: "white"
-            }
-        });
-    }
-
-    return(array);
-}
-
 const index = () => {
     const router = useRouter();
+    const [showLevels, setShowLevels] = useState(false);
+    const { width:screenWidth, height:screenHeight } = useWindowDimensions();
 
     return (
-        <SafeAreaProvider>
-            <SafeAreaView style={{ flex: 1 }}>
+        // <SafeAreaProvider>
+        //     <SafeAreaView style={{ flex: 1 }}>
                 <View style={styles.mainView}>
-                    <PopupScreen title="Levels" width={400} height={600} radius={10}>
-                        {/* <FlatList numColumns={4} style={{
-                            display: 'grid',
-                            flex: 1,
-                            margin: 25
-                        }}> */}
-                        <ItemSelectMenu items={makeItems(20)} width={400} height={450} numColumns={5}/>
-                        {/* </FlatList> */}
-                    </PopupScreen>
-                    <TitleScreenButton title="Play" onPress={() => router.push('/game')} />
-                    <View>
-                        <Text>Hello World</Text>
-                    </View>
+                    {
+                        showLevels ?
+                            <LevelSelect/>
+                        :
+                        <MainMenu onPlay={()=>setShowLevels(true)}/>
+                        
+                    }
+                    
                 </View>
-            </SafeAreaView>
-        </SafeAreaProvider>
+        //     </SafeAreaView>
+        // </SafeAreaProvider>
     );
 };
 
