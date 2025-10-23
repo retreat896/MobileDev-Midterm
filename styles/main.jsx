b9a8203fe9d83053c233cd5f2da9d20952bea553
Kris <33873725+retreat896@users.noreply.github.com>
2025-10-22 20:50:40 -0500

    fixed font. fixed packages.json using npx expo install --fix

--- styles/main.jsx
+++ styles/main.jsx
@@ -1,10 +1,5 @@
-
 import { useFonts } from '@expo-google-fonts/honk';
 import { StyleSheet } from 'react-native';
-
-// useFonts({
-//     'Honk': require('../node_modules/@expo-google-fonts/honk/400Regular/Honk_400Regular.ttf'),
-// });
 
 export const styles = StyleSheet.create({
     mainView: {
@@ -15,38 +10,20 @@ export const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'red',
     },
-    titleButton: {
-        width: 200,
-        height: 80,
-        backgroundColor: '#007276ff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 12,
-        marginVertical: 10,
-        borderColor: "red",
-        borderWidth: 2,
-    },
-    titleButtonText: {
-        color: 'white',
-        fontFamily:  'Honk',
-        fontSize: 48,
-        borderColor: "red",
-        borderWidth: 2,
-    },
     wrapper: {
-        position:"absolute",
-        width:"90%",
-        height:"90%",
-        flex:1,
-        opacity: 0.90,
-        borderColor: "red",
+        position: 'absolute',
+        width: '90%',
+        height: '90%',
+        flex: 1,
+        opacity: 0.9,
+        borderColor: 'red',
         borderWidth: 2,
     },
     row: {
         display: 'flex',
         flexDirection: 'row',
-        justifyContent:'center',
-        borderColor: "red",
+        justifyContent: 'center',
+        borderColor: 'red',
         borderWidth: 2,
-    }
+    },
 });
