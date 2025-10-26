ecfe3d4812896ee9a0e2fb4aabe8a1dbdc76304a
Jacob Malland <mallandj@uwplatt.edu>
2025-10-26 13:05:25 -0500

    Added fetch for leveldata

--- app/index.jsx
+++ app/index.jsx
@@ -9,11 +9,47 @@ import MainMenu from '@modules/MainMenu';
 import Wrapper from '@modules/Wrapper';
 import Level from '@app/Level';
 import * as ScreenOrientation from 'expo-screen-orientation';
+import leveldata from '@assets/leveldata.json';
 
 // router.push(path): Navigates to a new screen and adds it to the navigation stack.
 // router.replace(path): Replaces the current screen in the navigation stack with the new one.
 // router.back(): Navigates back to the previous screen.
 // router.navigate({ pathname: string, params: object }): Navigates to a specific path with parameters.
+
+// Array to hold all levels
+const levels = [];
+
+async function fetchLevels() {
+    let start = Date.now();
+    try {
+        // Process each level
+        for (let level of leveldata) {
+            // Fetch the Level image
+            let res = await fetch("https://raw.githubusercontent.com/retreat896/MobileDev-Midterm/main/" + level.image).catch(e => console.error)
+            
+            if (res.status !== 200) {
+                console.log(`Failed to resolve: "${res.url}`);
+                console.log(`Status: ${res.status}  -  "${res.statusText}"`);
+                
+                // Keep going
+                continue;
+            }
+
+            // Process the response data
+            let data = await res.json();
+        
+            // Create a new Level using the name and image
+            levels.push(new Level(level.name).setImage(data));
+            console.log(`Created Level: "${level.name}"`);
+        }
+    }
+    catch (e) {
+        console.error(e);
+    }
+    let end = Date.now();
+
+    console.log(`Level Load Time: ${(end - start)/1000} seconds`);
+}
 
 const index = () => {
     // Lock the screen to landscape mode
@@ -30,8 +66,9 @@ const index = () => {
 
     useEffect(() => {
         console.log("Loaded Main index.jsx")
-    }, [settings, stats, levelSelect])
 
+        fetchLevels();
+    }, [settings, stats, levelSelect])
 
     const showSettings = () => {
         if (settings) {
@@ -99,7 +136,7 @@ const index = () => {
                     }} >
                         
                     <LevelSelect levels={levels} 
-                        onSelect={(level) => {console.log("Level Selected: " + level.getName()); router.navigate("/Game")}}
+                        onSelect={(level) => {console.log("Level Selected: " + level.getName()); router.navigate("/game")}}
                         onChange={(level) => { setWrapperTitle(level.getName())}}
                     />
                 </Wrapper>
