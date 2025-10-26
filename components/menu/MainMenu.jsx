8ac15408c59b709b920e439307645654a2f43b9d
Jacob Malland <mallandj@uwplatt.edu>
2025-10-26 15:33:42 -0500

    Added Background to MainMenu

--- components/menu/MainMenu.jsx
+++ components/menu/MainMenu.jsx
@@ -1,17 +1,55 @@
-import { StyleSheet, View } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, View, ImageBackground, Dimensions } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import React from 'react';
 import CoolerButton from '@components/menu/CoolerButton';
 
-const MainMenu = ({onPlay, onSettings, onStats}) => {
+const { height: screenHeight, width: screenWidth } = Dimensions.get('window');
+const Background = require('@assets/backgrounds/used/Main_Background.jpg');
 
+const MainMenu = ({ onPlay, onSettings, onStats }) => {
     return (
-        <View>
-            <CoolerButton title="Play" onPress={onPlay||null} />
-            <CoolerButton title="Settings" onPress={onSettings||null} />
-            <CoolerButton title="Stats" onPress={onStats||null} />
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                {/* Background Layer */}
+                <ImageBackground
+                    source={Background}
+                    resizeMode="contain" // Changed to contain to show full image
+                    style={[
+                        styles.backgroundImage,
+                        styles.imageFullDisplay
+                    ]}
+                />
 
-        </View>
-    )
+                {/* Content Overlay Layer */}
+                <View style={styles.contentOverlay}>
+                    <CoolerButton title="Play" onPress={onPlay || null} />
+                    <CoolerButton title="Settings" onPress={onSettings || null} />
+                    <CoolerButton title="Stats" onPress={onStats || null} />
+                </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000', // Background color for letterboxing areas
+    },
+    backgroundImage: {
+        position: 'absolute',
+    },
+    imageFullDisplay: {
+        width: screenWidth,
+        height: screenHeight,
+        alignSelf: 'center',
+        justifyContent: 'center'
+    },
+    contentOverlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default MainMenu;
